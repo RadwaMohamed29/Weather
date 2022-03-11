b34cfb93fb55bdcdb8e8e49a44ea8fe2106beac7
radwa <radwamohamed2979@gmail.com>
2022-03-11 15:41:13 +0200

    dataBase source

--- app/src/main/java/com/example/weather/data/localData/DataSource.kt
+++ app/src/main/java/com/example/weather/data/localData/DataSource.kt
@@ -1,12 +1,20 @@
-//package com.example.weather.data.localData
+package com.example.weather.data.localData
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.weather.model.WeatherApi
+
+class DataSource {
+    var dao:Dao?
+
+    constructor(app:Application){
+        dao= Database.getWeatherDatabase(app)?.dao()
+    }
+//    fun getAllWeathers():LiveData<List<WeatherApi>>{
+//        return dao.getAllWeathers()
 //
-//import android.app.Application
-//
-//class DataSource {
-//    var dao:Dao
-//
-//    constructor(app:Application){
-//       // dao= Database.getWeatherDatabase(app)?.dao()
 //    }
-//
-//}
+
+
+
+}
